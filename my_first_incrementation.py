ad3fc7d3a3416fac0fc3efd466baa81ce0e161ed
Anthea Ip <antheaip964@gmail.com>
2024-01-30 13:19:45 -0500

    Saving

---
+++ my_first_incrementation.py
@@ -0,0 +1,17 @@
+'''Incrementation and decrementation depending on the language, it's either ++ (--) or += 1 (-= 1).
+
+Replace/Complete the following code.
+(The XX is what you need to replace)'''
+
+my_index = 0
+my_index += 1
+print(my_index)
+my_index -= 1
+my_index -= 1
+
+print(my_index)
+my_index += 1
+my_index += 1
+my_index += 1
+
+print(my_index)
---
+++ my_multiple_variables_multiple_type.py
@@ -0,0 +1,7 @@
+'''Replace/Complete the following code. Create multiple variables with (if needed) the right type.
+(The XX is what you need to replace)'''
+my_age = 34
+my_name = "Luke"
+my_comma = ','
+
+print("Hello " + my_name + my_comma + " I'm "+ str(my_age) + " years old.")
