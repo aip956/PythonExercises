b4047130dcf3a04babfbbb552e9200d5792e8279
Anthea Ip <antheaip964@gmail.com>
2024-01-30 13:29:57 -0500

    Saving

---
+++ Quest01/my_first_if_else.py
@@ -0,0 +1,15 @@
+'''my_first_if_else.py
+let earth_is_flat = false;
+
+if earth_is_flat {
+  println!("Science doesn't exist");
+} else {
+  println!("Phew.");
+}
+'''
+nbr = 10
+
+if (nbr > 20):
+  print("nbr is greater than 20")
+else:
+  print("nbr is less than 20")
