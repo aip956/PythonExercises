543a620669929c8b94978e8e8053e8c0b1f2e13b
Anthea Ip <antheaip964@gmail.com>
2024-01-30 13:09:38 -0500

    Saving

---
+++ my_first_variable_string.py
@@ -0,0 +1,4 @@
+'''Replace/Complete the following code. Create a variable with (if needed) the right type.
+(The XX is what you need to replace)'''
+my_string = "Learning is growing"
+print(my_string)
