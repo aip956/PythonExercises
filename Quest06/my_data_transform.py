1573e857038295ca2c683c3afc05ead437322ee7
Anthea Ip <antheaip964@gmail.com>
2024-01-31 17:28:49 -0500

    Saving

--- Quest06/my_data_transform.py
+++ Quest06/my_data_transform.py
@@ -2,15 +2,50 @@
 my_data_transform.py
 
 '''
+import csv
+from io import StringIO
 
 def my_data_transform(data):
     lines = data.split("\n")
-    header = lines[0]
-    result = header.split(",")
-    print(header)
+    header = lines[0].split(",")
+    result = [header]
 
-    result = [line.split(',') for line in lines]
+
+    for line in lines[1:]:
+        if not line:
+            continue
+        # row = line.split(',')
+        # Use  the csv module for parsing
+        row = next(csv.reader(StringIO(line)))
+        # skip line if empty columns
+        if len(row) < len(header):
+            continue
+        # convert age to integer
+        age = int(row[5])
+        if 1 <= age <= 20:
+            row[5] = '1->20'
+        elif 21 <= age <= 40:
+            row[5] = '21->40'
+        elif 41 <= age <= 65:
+            row[5] = '41->65'
+        elif 66 <= age <= 99:
+            row[5] = '66->99'
+        else:
+            row[5] = 'unknown'
+
+        # Time of day
+        order_time = int(row[-1].split()[1].split(':')[0])
+        if  6 <= order_time <= 12:
+            row[-1] = 'morning'
+        elif 12 <= order_time <= 18:
+            row[-1] = 'afternoon'
+        elif 18 <= order_time:
+            row[-1] = 'evening'
+        else: row[-1] = 'unknown'
+
+        result.append(row)
     
+
     if result[-1] == ['']:
         result.pop()
 
