7274c6c83fdb643ed863d54cabf9fda45acdd406
Anthea Ip <antheaip964@gmail.com>
2024-01-31 19:35:21 -0500

    Saving

---
+++ Quest06/my_data_process.py
@@ -0,0 +1,10 @@
+'''
+my_data_process.py
+Our function will group the data and it will become a Hash of hash. (Wow.)
+Example:
+"{'Gender': {'Male': 22, 'Female': 21}, 'Email': {'yahoo.com': 3, 'hotmail.com': 2}, ...}"
+
+We will discard the column FirstName, LastName, UserName and Coffee Quantity from our output.
+
+
+'''
