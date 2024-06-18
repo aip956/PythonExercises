2a6fc68750836e8e30c9497a3e88f983170b8785
Anthea Ip <antheaip964@gmail.com>
2024-06-18 19:42:53 -0400

    Working on TaskManager

---
+++ TaskManager/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:local@db:localhost/taskmanager"
+# pwd = local; username = postgres
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
