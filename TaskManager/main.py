b7681a19a361a163fcf7e5944ae785c92d256d8a
Anthea Ip <antheaip964@gmail.com>
2024-06-18 20:07:39 -0400

    Working on TaskManager

--- TaskManager/main.py
+++ TaskManager/main.py
@@ -22,3 +22,37 @@ def create_task(task: TaskCreate, db: Session = Depends(get_db)):
     send_notification(f'Task created: {db_task.title}')
     return db_task
 
+@app.get("/tasks/")
+def read_tasks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    tasks = db.query(Task).offset(skip).limit(limit).all()
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == task_id).first()
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task: TaskCreate, db: Session = Depends(get_db)):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    db_task.title = task.title
+    db_task.description = task.description
+    db.commit()
+    db.refresh(db_task)
+    send_notification(f'Task udpated: {db_task.title}')
+    return db_task
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    db.delete(db_task)
+    db.commit()
+    return {"ok": True}
+
+
