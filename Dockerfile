c4d429de7129448772079a24acf3e39264d0bff4
Anthea Ip <antheaip964@gmail.com>
2024-07-01 18:43:13 -0700

    Mock up of assignment works with FeelingIll, Catering, Waiters

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,22 @@
-# Dockerfile
+# # Dockerfile
 
-# official docker image
-FROM python:3.11.1-slim
+# # official docker image
+# FROM python:3.11.1-slim
 
-# set work directory
-WORKDIR /app
+# # set work directory
+# WORKDIR /app
 
-# set env variables
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+# # set env variables
+# ENV PYTHONDONTWRITEBYTECODE 1
+# ENV PYTHONUNBUFFERED 1
 
-# install dependencies
-COPY ./requirements.txt /app/requirements.txt 
-RUN pip install --upgrade pip --no-cache-dir --upgrade -r /app/requirements.txt
+# # install dependencies
+# COPY ./requirements.txt /app/requirements.txt 
+# RUN pip install --upgrade pip --no-cache-dir --upgrade -r /app/requirements.txt
 
-# copy project
-COPY ./app /app
+# # copy project
+# COPY ./app /app
 
-# run the application
-# CMD ["fastapi", "run", "app/main.py", "--port", "80"]
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+# # run the application
+# # CMD ["fastapi", "run", "app/main.py", "--port", "80"]
+# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -1,14 +1,108 @@
+# from fastapi import FastAPI, Response, BackgroundTasks, HTTPException
+# from aiokafka import AIOKafkaProducer, AIOKafkaConsumer, errors 
+# import asyncio
+# import logging
+
+# # KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "default_topic")
+# # KAFKA_BOOTSTRAP_SERVER = os.getenv("KAFKA_BOOTSTRAP_SERVER", "localhost:9092")
+
+# app = FastAPI()
+# logger = logging.getLogger("uvicorn.error")
+
+# loop = asyncio.get_event_loop()
+# producer = AIOKafkaProducer(loop=loop, bootstrap_servers='localhost:9092')
+
+# consumed_messages = []
+# filtered_messages = []
+
+# @app.on_event("startup")
+# async def on_startup():
+#     try:
+#         await producer.start()
+#         logger.info("Kafka producer started")
+#         asyncio.create_task(consume())
+#         logger.info("Kafka consumer task created")
+#     except errors.KafkaConnectionError as e:
+#         logger.error(f"Kafka connection error: {e}")
+
+
+# @app.on_event("shutdown")
+# async def on_shutdown():
+#     await producer.stop()
+#     logger.info("Kafka producer stopped")
+
+# @app.post("/send/{topic}")
+# async def produce(topic: str, message: str):
+#     try:
+#         await producer.send_and_wait(topic, message.encode('utf-8'))
+#         logger.info(f"Produced message: {message} to topic: {topic}")
+#         return {"message": "Message sent successfully"}
+#     except errors.KafkaConnectionError as e:
+#         logger.error(f"Kafka connection error: {e}")
+#         return {"message": "Failed to send message"}
+
+# async def consume():
+#     consumer = AIOKafkaConsumer(
+#         'my_topic',
+#         loop=loop,
+#         bootstrap_servers='localhost:9092',
+#         group_id="my-group")
+#     await consumer.start()
+#     try:
+#         async for msg in consumer:
+#             message = msg.value.decode('utf-8')
+#             topic = msg.topic
+#             consumed_messages.append({"topic": topic, "message": message})
+#             logger.info(f"Consumed message: {message} from topic: {topic}")
+#             # Filtering
+#             if "important" in message.lower():
+#                 filtered_messages.append({"topic": topic, "message": message})
+#                 logger.info(f"Filtered message: {message} from topic: {topic}")
+#     except Exception as e:
+#         logger.error(f"Error occurred: {e}")
+#     finally:
+#         await consumer.stop()
+
+
+# @app.get("/messages")
+# def get_messages():
+#     logger.info(f"Returning consumed messages: {consumed_messages}")
+#     return {"messages": consumed_messages}
+
+# @app.get("/filtered_messages")
+# def get_filtered_messages():
+#     logger.info(f"Returning filtered messages: {filtered_messages}")
+#     return {"messages": filtered_messages}
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+
+
+
+
+
+
+
+import time
 from fastapi import FastAPI, Response, BackgroundTasks, HTTPException
-from aiokafka import AIOKafkaProducer, AIOKafkaConsumer, errors 
+from aiokafka import AIOKafkaProducer, AIOKafkaConsumer, errors, AIOKafkaClient
 import asyncio
 import logging
 import os
 
-KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "default_topic")
+
+# KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "default_topic")
 KAFKA_BOOTSTRAP_SERVER = os.getenv("KAFKA_BOOTSTRAP_SERVER", "localhost:9092")
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn.error")
+
+# All the data topics
+topics = ["PersonFell", "BrokenGlass", "DirtyTable", "Brawl", "MissingRings", "MissingBride", "MissingGroom", "FeelingIll", "InjuredKid", "NotOnTheList", "BadFood", "MusicTooLoud", "MusicTooLow"]
 
 loop = asyncio.get_event_loop()
 producer = AIOKafkaProducer(loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVER)
@@ -22,6 +116,7 @@ async def on_startup():
     try:
         await producer.start()
         logger.info("Kafka producer started")
+        asyncio.create_task(consume_all())
         asyncio.create_task(consume_catering())
         asyncio.create_task(consume_waiters())
         logger.info("Kafka consumer tasks catering and waiters created")
@@ -43,7 +138,24 @@ async def produce(topic: str, message: str):
         logger.error(f"Kafka connection error: {e}")
         return {"message": "Failed to send message"}
 
-# Kafka topics and bootstrap servers are hardcoded; address this
+async def consume_all():
+    consumer = AIOKafkaConsumer(
+        *topics, # Unpack all topic names
+        loop=loop,
+        bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
+        group_id= "AllConsumers")
+    await consumer.start()
+    try:
+        async for msg in consumer:
+            message = msg.value.decode('utf-8')
+            topic = msg.topic
+            consumed_messages.append({"topic": topic, "message": message})
+            logger.info(f"Consumed_all message: {message} from topic: {topic}")
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+    finally:
+        await consumer.stop()
+
 async def consume_catering():
     consumer = AIOKafkaConsumer(
         'FeelingIll', #Sub in KAFA_TOPIC later
@@ -86,7 +198,7 @@ async def consume_waiters():
 @app.get("/messages")
 def get_messages():
     logger.info(f"Returning consumed messages: {consumed_messages}")
-    return {"messages": consumed_messages}
+    return {"All messages": consumed_messages}
 
 @app.get("/waiters_messages")
 def get_waiters_messages():
