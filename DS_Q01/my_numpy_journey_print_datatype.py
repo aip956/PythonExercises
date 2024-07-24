f9bd9b8a83c3944c534be23c0cbf1d396f2f32ff
Anthea Ip <antheaip964@gmail.com>
2024-07-23 21:41:07 -0400

    DS

--- DS_Q01/my_numpy_journey_print_datatype.py
+++ DS_Q01/my_numpy_journey_print_datatype.py
@@ -1,8 +1,25 @@
 import numpy as np
 
-def my_numpy_journey_print_datatype(input):
-    # print("Data type: ", np.array(input).dtype)
-    return np.array(input).dtype
+# def my_numpy_journey_print_datatype(input):
+    
+#     np_input = np.array(input, dtype=str)
+#     print("Data type: ", np.array(np_input).dtype)
+#     return np.array(input).dtype
 
-my_numpy_journey_print_datatype([[]])
-# my_numpy_journey_print_datatype([[1, 2, 3, 4], [0, 1, 1, 1], [7, 7, 7, 7]])
+
+def my_numpy_journey_print_datatype(input_list):
+    # Convert the entire input list to a string representation
+    input_str = str(input_list)
+    
+    # Create a NumPy array from this string, ensuring it's treated as a single element array of strings
+    np_input = np.array([input_str])
+    
+    # Print the dtype of the resulting array
+    print("Data: ", np_input.dtype)
+    
+    return np_input.dtype
+
+# # print(my_numpy_journey_print_datatype([[]]))
+# # print(my_numpy_journey_print_datatype([[1, 2, 3, 4], [0, 1, 1, 1], [7, 7, 7, 7]]))
+my_numpy_journey_print_datatype(([[]]))
+my_numpy_journey_print_datatype([[1, 2, 3, 4], [0, 1, 1, 1], [7, 7, 7, 7]])
