4692cccebbd10b9d189bd2a9d3f4c94d773f6515
Anthea Ip <antheaip964@gmail.com>
2024-08-22 21:33:15 -0400

    Practice

--- Problems/rcap.py
+++ Problems/rcap.py
@@ -6,11 +6,24 @@
 
 # A letter is a character in the set [a-zA-Z]
 # For word in string, for word[-1]
-
+import re
 def rcapitalize(param):
-    words = param.split()
+    words = re.split(r'(\s+)', param)
+    new_str = []
     for word in words:
-        print("Word: ", word)
-        print("word[-1]: ", word[-1])
+        # if len(word) > 1:
+            # print("Word: ", word)
+            # print("word[-1]: ", word[-1]
+        if word.strip(): # Check if the word is not just white space
+            if len(word) > 1:
+                new_word = word[:-1].lower() + word[-1].upper()
+            else:
+                new_word = word.upper()
+            # print("word[-1] cap: ", new_word)
+        else:
+            new_word = word # Preserve the spaces as is
+        new_str.append(new_word)
+    result = ''.join(new_str)
+    print(result)
 
 rcapitalize("a FiRSt LiTTlE TESt")
