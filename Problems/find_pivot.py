e31fe42e517a821831d03603931bce88ebc29da5
Anthea Ip <antheaip964@gmail.com>
2024-08-30 21:18:06 -0400

    Python

---
+++ Problems/find_pivot.py
@@ -0,0 +1,11 @@
+"""
+Given an array of integers and its size passed as parameters,
+create a function able to return the pivot index of this array.
+The pivot index is the index where the sum of the numbers on the left
+is equal to the sum of the numbers on the right.
+
+C Prototype:
+
+int	find_pivot(int *arr, int n);
+If there is no pivot present, return -1.
+"""
