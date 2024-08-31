e13bdc1e2c1ebb4380151d96afb4877b30ebdd77
Anthea Ip <antheaip964@gmail.com>
2024-08-30 21:23:50 -0400

    Python

--- Problems/find_pivot.py
+++ Problems/find_pivot.py
@@ -8,4 +8,16 @@ C Prototype:
 
 int	find_pivot(int *arr, int n);
 If there is no pivot present, return -1.
+Input: [1, 2, 3, 4, 0, 6] && 6
+Output: 3
+sum from_left and from_right, index from_left and from_right
+while 
+if from_left_sum < from_right_sum, increment from_left_index_index
+else if from_left > from_right, decriment from_right_index
+else if equal, increment and decriment both indices and sums
+
 """
+
+def find_pivot(param_1, param_2):
+
+    while from_left
