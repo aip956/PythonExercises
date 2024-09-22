d95a27e8143f0aa7beea556637dac9eb05db001e
Anthea Ip <antheaip964@gmail.com>
2024-09-21 20:23:52 -0400

    Saving

--- Problems/alpha_mirror.py
+++ Problems/alpha_mirror.py
@@ -4,4 +4,14 @@ def alpha_mirror(param_1):
     output = ""
     for letter in param_1:
         if letter.isupper():
-            output.append()
+            new_letter = letter.lower()
+            output += new_letter
+            print("9output: ", output)
+        elif letter.islower():
+            new_letter = letter.upper()
+            output += new_letter
+            print("13output: ", output)
+    return output
+
+input1 = "abc"
+        
