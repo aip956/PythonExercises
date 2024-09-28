925b59fef3eae978e39bda42674dff1f43aca6e8
Anthea Ip <antheaip964@gmail.com>
2024-09-28 14:04:33 -0400

    Python

---
+++ DS_Q04_Sklearn/my_model_evaluation_journey_confusion_matrix.py
@@ -0,0 +1,13 @@
+"""
+Evaluate the model prediction model for a data set using confusion matrix method.
+
+n the field of machine learning and specifically the problem of statistical classification, a confusion matrix, also known as an error matrix, is a specific table layout that allows visualization of the performance of an algorithm, typically a supervised learning one (in unsupervised learning it is usually called a matching matrix). Each row of the matrix represents the instances in a predicted class while each column represents the instances in an actual class (or vice versa). The name stems from the fact that it makes it easy to see if the system is confusing two classes (i.e. commonly mislabeling one as another).
+
+It is a special kind of contingency table, with two dimensions ("actual" and "predicted"), and identical sets of "classes" in both dimensions (each combination of dimension and class is a variable in the contingency table).
+
+Create a function my_model_evaluation_journey_confusion_matrix which will calculate the Confusion Matrix for a predicted model.
+
+You will receive two arguments: true data and predicted data. Both of them are integers.
+
+"""
+
