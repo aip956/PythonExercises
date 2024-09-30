e0fe53721155033fc735bc02e9c72b0081d14582
Anthea Ip <antheaip964@gmail.com>
2024-09-29 21:54:28 -0400

    Python

---
+++ Problems/my_array_numbers.py
@@ -0,0 +1,9 @@
+"""
+Create a function my_array_numbers which 
+creates and returns an array with integers 
+from 0 to 14 (0 and 14 included).
+"""
+
+def my_array_numbers():
+    return list(range(15))
+print(my_array_numbers()) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
