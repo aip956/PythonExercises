c5142dce4f5ca198c9e17956ac2eefe1ccd92ea2
Anthea Ip <antheaip964@gmail.com>
2024-10-01 22:42:42 -0400

    Python

--- DS_Q04_Sklearn/README.md
+++ DS_Q04_Sklearn/README.md
@@ -1,5 +1,12 @@
 Enter venv:
 Change directory (to Exercises)
 
-source/venv/bin/activate
+create venv: python3 -m venv new_venv
 
+activate the venv: source new_venv/bin/activate
+
+install pip: 
+python3 -m pip install --upgrade pip
+
+install scikit-learn:
+pip install scikit-learn
--- DS_Q04_Sklearn/my_model_evaluation_journey_accuracy_score.py
+++ DS_Q04_Sklearn/my_model_evaluation_journey_accuracy_score.py
@@ -23,3 +23,20 @@ def my_model_evaluation_journey_accuracy_score(param_1, param_2):
 
 '''
 
+from sklearn.metrics import accuracy_score
+def my_model_evaluation_journey_accuracy_score(true_data_list, pred_data_list):
+    try:
+        # Do both lists have the same length?
+        if len(true_data_list) != len(pred_data_list):
+            return False
+        accuracy = accuracy_score(true_data_list, pred_data_list)
+        print("accuracy: ", accuracy)
+        return True
+    except Exception as e:
+        print("An error occurred: ", e)
+        return False
+    
+true_data = [2, 0, 2, 2, 0, 1, 1, 2, 2, 0, 1, 2]
+pred_data = [0, 0, 2, 1, 0, 2, 1, 0, 2, 0, 2, 2]
+result = my_model_evaluation_journey_accuracy_score(true_data, pred_data) # True
+print("result: ", result)
