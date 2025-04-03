b24b3983d195ba46cdea97dd3b32a0789b886240
Anthea Ip <antheaip964@gmail.com>
2025-04-02 23:18:01 -0400

    Problems

---
+++ Problems/Implementation/BeautifulDay.py
@@ -0,0 +1,22 @@
+def reverse(num):
+    rev = 0
+    while num > 0:
+        rev *= 10
+        mod = num % 10
+        rev += mod
+        num //= 10
+    return rev
+    
+def beautiful_days(i, j, k):
+    count = 0
+    for a in range(i, j + 1):
+        rev = reverse(a)
+        if abs(a - rev) % k == 0:
+            count += 1
+    return count
+
+if __name__ == "__main__":
+    i = 20
+    j = 23
+    k = 6
+    beautiful_days(i, j, k)
