59b9cddb88cb731e1acba2448c514a21fcf63d9e
Anthea Ip <antheaip964@gmail.com>
2025-04-07 16:17:39 -0700

    Implementation

---
+++ Problems/Implementation/LibraryFine.py
@@ -0,0 +1,15 @@
+
+
+def libraryFine(d1, m1, y1, d2, m2, y2):
+    # Write your code here
+    
+
+if __name__ == '__main__':
+    # Return date
+    d1 = 9
+    m1 = 6
+    y1 = 2015
+    # Due Date
+    d2 = 6
+    m2 = 6
+    y2 = 2015
