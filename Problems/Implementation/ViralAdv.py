af97ff8beb03b858f1fb904ad9588028c5909a59
Anthea Ip <antheaip964@gmail.com>
2025-04-09 15:19:10 -0700

    Implementation

---
+++ Problems/Implementation/ViralAdv.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+'''
+https://www.hackerrank.com/challenges/strange-advertising/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
+Day 1: 
+
+'''
