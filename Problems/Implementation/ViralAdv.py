93244fca9f4fa79911b68807f1d8db9d9284dc37
Anthea Ip <antheaip964@gmail.com>
2025-04-09 15:25:24 -0700

    Implementation

--- Problems/Implementation/ViralAdv.py
+++ Problems/Implementation/ViralAdv.py
@@ -7,5 +7,15 @@
 https://www.hackerrank.com/challenges/strange-advertising/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
 
 Day 1: 
+Send to 5
+Like: Floor 5/2 = 2
+The 2 share with 3
+Day 2:
+Send to Floor 5/2 * 3 = 6
+Like: Floor recipients / 2
+Share with 3
+
+Determine how many people like the ad on a given day
+n = 5
 
 '''
