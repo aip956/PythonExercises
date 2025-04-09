656cfb98a67b187bd4a863a4ce389e4b2d4becda
Anthea Ip <antheaip964@gmail.com>
2025-04-09 15:50:45 -0700

    Implementation

--- Problems/Implementation/ViralAdv.py
+++ Problems/Implementation/ViralAdv.py
@@ -1,5 +1,11 @@
 
+def viralAdvertising(n):
+    
+    # Write your code here
 
+if __name__ == '__main__':
+    n = 5
+    viralAdvertising(n)
 
 
 
@@ -7,15 +13,30 @@
 https://www.hackerrank.com/challenges/strange-advertising/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
 
 Day 1: 
-Send to 5
+Share: Send to 5
 Like: Floor 5/2 = 2
-The 2 share with 3
+CummulativeLikes = 2
+Share: The 2 share with 3
 Day 2:
-Send to Floor 5/2 * 3 = 6
-Like: Floor recipients / 2
-Share with 3
+Share: Send to Floor 5/2 * 3 = 6
+Like: Floor recipients / 2 = 3
+CummulativeLikes = 5
+Day 3:
+Share with 3 * Likes = 9
+Like: Floor recipients / 2 = 9/2 = 4
+CummulativeLikes = 5+ 4 = 9
+Day 4:
+Share: 3 * 4 = 12
+Likes = Floor recipients / 2 = 12/2 = 6
+CummulativeLikes = 9 + 6 = 15
+Day 5:
+Share: 3 * Likes(6) = 18
+Likes = Floor recipients / 2 = 18/2 = 9
+CummulativeLikes = 15 + 9 = 24
 
 Determine how many people like the ad on a given day
 n = 5
+
+
 
 '''
